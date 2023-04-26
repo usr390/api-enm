4369bfa20f81625636effa7e789857335142f5ee
vendingcompute <edutre1999@gmail.com>
2023-04-26 12:26:53 -0600

    basic hello world

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-console.log(process.env.PORT)
+import express from "express";
+const app = express()
+const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+res.send('Hello Worldd!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
