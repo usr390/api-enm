cb9052e8bdbaead3a03e17a833446c31e50f922a
vendingcompute <edutre1999@gmail.com>
2023-04-27 15:17:14 -0600

    created mock enmEvents. varied in terms of size

--- src/index.ts
+++ src/index.ts
@@ -96,6 +96,76 @@ app.get('/api/enmEvents', (req: Request, res: Response) => {
         "day": "17",
         "month": "June",
         "year": "2023"
+      },
+      {
+        "id": 7,
+        "name": "Bojira at McAllen Convention Center",
+        "description": "Ticket can be purchased on ticketmaster",
+        "address": "700 Convention Center Blvd",
+        "city": "McAllen",
+        "state": "Texas",
+        "country": "USA",
+        "start": "7:00pm",
+        "end": "9:30",
+        "day": "30",
+        "month": "Aug",
+        "year": "2023"
+      },
+      {
+        "id": 8,
+        "name": "GLAM * FUSE * GLARE * DEFEATED NOTION",
+        "description": "Killer line up",
+        "address": "808 Drum Drive",
+        "city": "Pharr",
+        "state": "Texas",
+        "country": "USA",
+        "start": "7:00pm",
+        "end": "9:30",
+        "day": "29",
+        "month": "May",
+        "year": "2023"
+      },
+      {
+        "id": 9,
+        "name": "Music Concert",
+        "description": "Concert",
+        "address": "111 Simple Event Avenue",
+        "city": "Los Fresnos",
+        "state": "Texas",
+        "country": "USA",
+        "start": "6:00pm",
+        "end": "8:30",
+        "day": "29",
+        "month": "July",
+        "year": "2023"
+      },
+      {
+        "id": 10,
+        "name": "Noodlefest",
+        "description": "The iconic noodlefest returns with an amazing lineup as always. Move Kant Adjust, Cure Division, Lunar Eye, and more. Check out our art vendors for cool, one of a kind items only found here in the valley. Also be sure to order some amazing grub from our many street food artist we'll have posted up at the event. This includes Gard's amazing deep fried noodles.. as if we thought we'd seen it all! This is a safe and inclusive space for our community. Look out for each other. Racism, transphobia, homophobia, sexism, or any other type of discrimination or bigotry is NOT TOLERATED. If something is messed up, say something. Security/Staff and we will handle it as quickly and diligently as possible",
+        "address": "1300 Ty Grovve Blvd",
+        "city": "Edinburg",
+        "state": "Texas",
+        "country": "USA",
+        "start": "8pm",
+        "end": "11pm",
+        "day": "29",
+        "month": "September",
+        "year": "2023"
+      },
+      {
+        "id": 11,
+        "name": "The Shack Presents - Calm Bull, Cat Bamboo, The Decemberists, Forest Bodies, King Van",
+        "description": "Line up like no other at The Shack this coming weekend. Calm Bull opening the show with the eccentric, genre-bending style they are critically acclaimed for. Taking influence from the underground post hardcore scene which bloomed in the late 90s they've been able to take the sound and morph it into something truly air rippling. Don't miss it. Directly following their set we have some familiar guests. Cat Bamboo. Midway through their United States tour they take the time to play out some of their most resonating pieces with their math rockish approach to rythms and chords. It'll be a treat for those in attendance. Of course, we'd be remiss not adverting your attention to one of the greatest to ever do it in the valley's alt scene. The Septemberists. Known for their energetic sets in the mid to late 00's they come back to give the scene something time has made forcibly forgotten: the driving, colorful sounds of their live sets. On that same note we have Forest Bodies. Long ago in the deep recesses of valley neighborhoods, bars, and venues resonated the rythmic sounds of this group. Now, The Shack has arranged for their return. New walls will contain and reflect the sounds this legendary valley band produced. All for the ear to consume. Lastly, the closing act: King Van. Not sure we need to say anything to be completely honest. We will simply for the fact that King Van's utterly atrocious sonic pallete is something to truly behold. Wait what? Atrocious? Yes. The wall of sheer abrasive air coming from their amps will have anyone in the vicinity tranced by the abhorrently beautiful melodics, drones, and atmospheric backing tracks. Expose your sensory interface to something it will struggle to understand. Completely worth it.",
+        "address": "333 Angel Numbers Road",
+        "city": "Brownsville",
+        "state": "Texas",
+        "country": "USA",
+        "start": "7:30pm",
+        "end": "10pm",
+        "day": "1",
+        "month": "January",
+        "year": "2022"
       }
     ]
   );
